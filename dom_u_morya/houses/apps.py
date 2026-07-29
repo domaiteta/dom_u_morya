477c3835edf5067a2f9e516a0539cdf509527c7c
domai <dmitry.sinyak@it-solutions.pro>
2026-07-29 21:15:28 +0500

    add meta info

--- dom_u_morya/houses/apps.py
+++ dom_u_morya/houses/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class HousesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'houses'
+
+    verbose_name = 'Дом'
+    verbose_name_plural = 'Дома'
--- dom_u_morya/houses/models.py
+++ dom_u_morya/houses/models.py
@@ -4,3 +4,7 @@ class House(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField(verbose_name="Цена")
     description = models.TextField(verbose_name="Описание")
+
+    class Meta:
+        verbose_name = 'Дом'
+        verbose_name_plural = 'Дома'
