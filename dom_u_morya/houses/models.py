ece10dc24e221c469fbadf36d1a8972d4fa71813
domai <dmitry.sinyak@it-solutions.pro>
2026-07-29 20:55:34 +0500

    add house model

--- dom_u_morya/houses/models.py
+++ dom_u_morya/houses/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class House(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField(verbose_name="Цена")
+    description = models.TextField(verbose_name="Описание")
